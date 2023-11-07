6d0e120167c780c0f91d39716f67d51c7416b0a1
dappnodedev <diego@dappnode.io>
2023-11-07 11:04:37 +0100

    Remove MEV Boost

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -22,18 +22,6 @@ case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_HOLESKY in
     ;;
 esac
 
-# MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
-if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_HOLESKY" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_HOLESKY" == "true" ]; then
-    echo "MEV-Boost is enabled"
-    MEVBOOST_URL="http://mev-boost.mev-boost-holesky.dappnode:18550"
-    if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
-        EXTRA_OPTS="--builder --builder.urls=${MEVBOOST_URL} ${EXTRA_OPTS}"
-    else
-        echo "MEV-Boost is enabled but the Holesky MEV-Boost package at ${MEVBOOST_URL} is not reachable"
-        curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} can not be reached" --data-urlencode 'body=Make sure the Holesky MEV-Boost DNP is available and running'
-    fi
-fi
-
 exec node --max-old-space-size=${MEMORY_LIMIT} /usr/app/node_modules/.bin/lodestar \
     beacon \
     --network=holesky \
